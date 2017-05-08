00e36206f4021fc652fcb5345901dc47e3cab2da
Or Avner <or@vsecgroup.com>
2017-05-09 00:20:05 +0300

    now uses NULL instead of nullptr; fixed ctor warning

--- wet1/AVLTree.h
+++ wet1/AVLTree.h
@@ -2,6 +2,7 @@
 #define _AVLTREE_
 
 #include <math.h>
+#include <iostream>
 
 class AVLTreeException {};
 class AVLTreeKeyNotFoundException : public AVLTreeException {};
@@ -31,7 +32,7 @@ public:
 	AVLTree(ArrayNode* array, int size);
 	static ArrayNode* tree_to_array(const AVLTree& tree);
 	// O(1)
-	AVLTree() : _root(nullptr), _size(0), _biggest(nullptr) {};
+	AVLTree() : _root(NULL), _last_searched(NULL), _biggest(NULL), _size(0) {};
 
 	// O(log n)
 	void insert(KeyType const & key, ValueType const & value);
@@ -52,21 +53,21 @@ protected:
 		Node* _left_son;
 		Node* _right_son;
 		int _height;
-		Node(KeyType const & key, ValueType const & value) : _key(key), _value(value), _parent(nullptr), _left_son(nullptr), _right_son(nullptr), _height(0) {}
+		Node(KeyType const & key, ValueType const & value) : _key(key), _value(value), _parent(NULL), _left_son(NULL), _right_son(NULL), _height(0) {}
 		Node() = default;
 		void update_height() {
-			if (_left_son == nullptr && _right_son == nullptr) _height = 0;
-			else if (_right_son == nullptr) _height = _left_son->_height + 1;
-			else if (_left_son == nullptr) _height = _right_son->_height + 1;
+			if (_left_son == NULL && _right_son == NULL) _height = 0;
+			else if (_right_son == NULL) _height = _left_son->_height + 1;
+			else if (_left_son == NULL) _height = _right_son->_height + 1;
 			else {
 				if (_left_son->_height >= _right_son->_height) _height = _left_son->_height + 1;
 				else _height = _right_son->_height + 1;
 			}
 		}
 		int bf() const {
-			if (_left_son == nullptr && _right_son == nullptr) return 0;
-			else if (_right_son == nullptr) return _left_son->_height + 1;
-			else if (_left_son == nullptr) return -(_right_son->_height + 1);
+			if (_left_son == NULL && _right_son == NULL) return 0;
+			else if (_right_son == NULL) return _left_son->_height + 1;
+			else if (_left_son == NULL) return -(_right_son->_height + 1);
 			return _left_son->_height - _right_son->_height;
 		}
 	};
@@ -109,7 +110,7 @@ protected:
 template<typename KeyType, typename ValueType>
 typename AVLTree<KeyType, ValueType>::Node* AVLTree<KeyType, ValueType>::find_recursion(KeyType const & key, Node * node)
 {
-	if (node == nullptr) throw AVLTreeKeyNotFoundException();
+	if (node == NULL) throw AVLTreeKeyNotFoundException();
 	_last_searched = node;
 	if (node->_key == key) return node;
 	else if (node->_key < key) return find_recursion(key, node->_right_son);
@@ -119,7 +120,7 @@ typename AVLTree<KeyType, ValueType>::Node* AVLTree<KeyType, ValueType>::find_re
 template<typename KeyType, typename ValueType>
 typename AVLTree<KeyType, ValueType>::Node ** AVLTree<KeyType, ValueType>::parent_to_son_link_pointer(Node const * const son)
 {
-	if (son->_parent == nullptr) return &_root;
+	if (son->_parent == NULL) return &_root;
 	if (son->_parent->_left_son == son) {
 		return &(son->_parent->_left_son);
 	}
@@ -134,13 +135,13 @@ void AVLTree<KeyType, ValueType>::swap_nodes(Node & a, Node & b)
 	*(parent_to_son_link_pointer(&b)) = &a;
 	*(parent_to_son_link_pointer(&a)) = &b;
 	a._left_son = b._left_son;
-	if (a._left_son != nullptr) a._left_son->_parent = &a;
+	if (a._left_son != NULL) a._left_son->_parent = &a;
 	b._left_son = left_son_t;
-	if (b._left_son != nullptr) b._left_son->_parent = &b;
+	if (b._left_son != NULL) b._left_son->_parent = &b;
 	a._right_son = b._right_son;
-	if (a._right_son != nullptr) a._right_son->_parent = &a;
+	if (a._right_son != NULL) a._right_son->_parent = &a;
 	b._right_son = right_son_t;
-	if (b._right_son != nullptr) b._right_son->_parent = &b;
+	if (b._right_son != NULL) b._right_son->_parent = &b;
 	a._parent = b._parent;
 	b._parent = parent_t;
 	a._height = b._height;
@@ -191,7 +192,7 @@ void AVLTree<KeyType, ValueType>::ll_rotation(Node * b)
 	a->_parent = b->_parent;
 	b->_parent = a;
 	b->_left_son = a->_right_son;
-	if (a->_right_son != nullptr) a->_right_son->_parent = b;
+	if (a->_right_son != NULL) a->_right_son->_parent = b;
 	a->_right_son = b;
 	b->update_height();
 	a->update_height();
@@ -212,7 +213,7 @@ void AVLTree<KeyType, ValueType>::rr_rotation(Node * b)
 	a->_parent = b->_parent;
 	b->_parent = a;
 	b->_right_son = a->_left_son;
-	if (a->_left_son != nullptr) a->_left_son->_parent = b;
+	if (a->_left_son != NULL) a->_left_son->_parent = b;
 	a->_left_son = b;
 	b->update_height();
 	a->update_height();
@@ -229,15 +230,15 @@ template<typename KeyType, typename ValueType>
 void AVLTree<KeyType, ValueType>::update_biggest()
 {
 	_biggest = _root;
-	if (_biggest != nullptr) {
-		while (_biggest->_right_son != nullptr) _biggest = _biggest->_right_son;
+	if (_biggest != NULL) {
+		while (_biggest->_right_son != NULL) _biggest = _biggest->_right_son;
 	}
 }
 
 template<typename KeyType, typename ValueType>
 void AVLTree<KeyType, ValueType>::delete_recursive(Node * node)
 {
-	if (node == nullptr) return;
+	if (node == NULL) return;
 	delete_recursive(node->_left_son);
 	delete_recursive(node->_right_son);
 	delete node;
@@ -246,7 +247,7 @@ void AVLTree<KeyType, ValueType>::delete_recursive(Node * node)
 template<typename KeyType, typename ValueType>
 void AVLTree<KeyType, ValueType>::add_to_array_recursion(Node * node, ArrayNode* array, int * offset)
 {
-	if (node == nullptr) return;
+	if (node == NULL) return;
 	add_to_array_recursion(node->_left_son, array, offset);
 	*(array + *offset) = ArrayNode(node->_key, node->_value);
 	*offset = *offset + 1;
@@ -256,7 +257,7 @@ void AVLTree<KeyType, ValueType>::add_to_array_recursion(Node * node, ArrayNode*
 template<typename KeyType, typename ValueType>
 void AVLTree<KeyType, ValueType>::add_from_array_recursion(Node * node, ArrayNode* array, int * offset)
 {
-	if (node == nullptr) return;
+	if (node == NULL) return;
 	add_from_array_recursion(node->_left_son, array, offset);
 	ArrayNode* array_node = (array + *offset);
 	node->_key = array_node->_key;
@@ -277,14 +278,14 @@ typename AVLTree<KeyType, ValueType>::ArrayNode* AVLTree<KeyType, ValueType>::tr
 template<typename KeyType, typename ValueType>
 typename AVLTree<KeyType, ValueType>::Node * AVLTree<KeyType, ValueType>::complete_tree(int height)
 {
-	if (height == -1) return nullptr;
+	if (height == -1) return NULL;
 	Node* left_son = complete_tree(height - 1);
 	Node* right_son = complete_tree(height - 1);
 	Node* node = new Node();
 	node->_left_son = left_son;
 	node->_right_son = right_son;
-	if (left_son != nullptr) left_son->_parent = node;
-	if (right_son != nullptr) right_son->_parent = node;
+	if (left_son != NULL) left_son->_parent = node;
+	if (right_son != NULL) right_son->_parent = node;
 	node->_height = height;
 	return node;
 }
@@ -292,14 +293,14 @@ typename AVLTree<KeyType, ValueType>::Node * AVLTree<KeyType, ValueType>::comple
 template<typename KeyType, typename ValueType>
 void AVLTree<KeyType, ValueType>::minimize_complete_tree(Node* node, int final_size, int *current_size)
 {
-	if (node == nullptr) return;
+	if (node == NULL) return;
 	if (*current_size == final_size) return;
 	if (node->_height == 0) {
 		if (node->_parent->_left_son == node) {
-			node->_parent->_left_son = nullptr;
+			node->_parent->_left_son = NULL;
 		}
 		else {
-			node->_parent->_right_son = nullptr;
+			node->_parent->_right_son = NULL;
 		}
 		node->_parent->update_height();
 		delete node;
@@ -329,7 +330,7 @@ AVLTree<KeyType, ValueType>::AVLTree(ArrayNode* array, int size)
 template<typename KeyType, typename ValueType>
 void AVLTree<KeyType, ValueType>::insert(KeyType const & key, ValueType const & value)
 {
-	if (_root == nullptr) {
+	if (_root == NULL) {
 		_root = new Node(key, value);
 		_biggest = _root;
 		_size++;
@@ -358,19 +359,19 @@ template<typename KeyType, typename ValueType>
 void AVLTree<KeyType, ValueType>::erase(KeyType const & key)
 {
 	Node* node_to_delete = find_recursion(key, _root);
-	if (node_to_delete->_left_son != nullptr && node_to_delete->_right_son != nullptr) {
+	if (node_to_delete->_left_son != NULL && node_to_delete->_right_son != NULL) {
 		Node* next_minimal_node = node_to_delete->_right_son;
-		while (next_minimal_node->_left_son != nullptr) {
+		while (next_minimal_node->_left_son != NULL) {
 			next_minimal_node = next_minimal_node->_left_son;
 		}
 		swap_nodes(*node_to_delete, *next_minimal_node);
 	} 
 
 	Node** parent_to_son_pointer = parent_to_son_link_pointer(node_to_delete);
-	if (node_to_delete->_left_son == nullptr && node_to_delete->_right_son == nullptr) {
-		*parent_to_son_pointer = nullptr;
+	if (node_to_delete->_left_son == NULL && node_to_delete->_right_son == NULL) {
+		*parent_to_son_pointer = NULL;
 	}
-	else if (node_to_delete->_left_son == nullptr) {
+	else if (node_to_delete->_left_son == NULL) {
 		*parent_to_son_pointer = node_to_delete->_right_son;
 		node_to_delete->_right_son->_parent = node_to_delete->_parent;
 	}
@@ -379,7 +380,7 @@ void AVLTree<KeyType, ValueType>::erase(KeyType const & key)
 		node_to_delete->_left_son->_parent = node_to_delete->_parent;
 	}
 
-	if (node_to_delete->_parent != nullptr) {
+	if (node_to_delete->_parent != NULL) {
 		balance_bottom_up(node_to_delete);
 	}
 	if (_biggest == node_to_delete) {
@@ -392,7 +393,7 @@ void AVLTree<KeyType, ValueType>::erase(KeyType const & key)
 template<typename KeyType, typename ValueType>
 ValueType & AVLTree<KeyType, ValueType>::biggest() const
 {
-	if (_biggest == nullptr) throw AVLTreeIsEmpty();
+	if (_biggest == NULL) throw AVLTreeIsEmpty();
 	return _biggest->_value;
 }
 
