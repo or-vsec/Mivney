c6e057733fe0fe0d0d265a7e542c6ee312719059
Or Avner <or@vsecgroup.com>
2017-06-09 18:55:22 +0300

    union find now support value template

--- wet2/UnionFind.h
+++ wet2/UnionFind.h
@@ -4,13 +4,14 @@
 
 class UnionFindException {};
 class UnionFindKeyNotFoundException : public UnionFindException {};
-class UnionFindSameGroupExpcetion : public UnionFindException {};
+class UnionFindSameGroupException : public UnionFindException {};
 
+template<typename ValueType>
 class UnionFind {
 
 public:
-	int Find(const int key);
-	int Union(const int group_1, const int group_2);
+	ValueType& Find(const int key);
+	ValueType& Union(const int group_1, const int group_2);
 
 	UnionFind(int size);
 	~UnionFind();
@@ -19,9 +20,8 @@ protected:
 
 	class Node {
 	public:
-		int _key;
+		ValueType _key;
 		Node* _parent;
-		Node(int key) : _key(key), _parent(NULL) {}
 		Node() : _parent(NULL) {}
 	};
 
@@ -32,7 +32,6 @@ protected:
 		GroupNode(Node* node) : _node(node), _size(1) {}
 	};
 
-
 	// Members
 	Node** _nodes;
 	GroupNode** _groups;
@@ -40,7 +39,8 @@ protected:
 	int _size;
 };
 
-int UnionFind::Find(const int key)
+template<typename ValueType>
+ValueType& UnionFind<ValueType>::Find(const int key)
 {
 	if (key > _size || key <= 0) throw UnionFindKeyNotFoundException();
 	Node* node = _nodes[key-1];
@@ -58,12 +58,14 @@ int UnionFind::Find(const int key)
 	return root->_key;
 }
 
-int UnionFind::Union(const int group_1, const int group_2)
+
+template<typename ValueType>
+ValueType& UnionFind<ValueType>::Union(const int group_1, const int group_2)
 {
 	if (group_1 > _size || group_2 > _size || group_1 <= 0 || group_2 <= 0) throw UnionFindKeyNotFoundException();
 	GroupNode* group1 = _groups[group_1-1];
 	GroupNode* group2 = _groups[group_2-1];
-	if (group1 == group2) throw UnionFindSameGroupExpcetion();
+	if (group1 == group2) throw UnionFindSameGroupException();
 	if (group1->_size >= group2->_size) {
 		group1->_size += group2->_size;
 		group2->_node->_parent = group1->_node;
@@ -78,20 +80,22 @@ int UnionFind::Union(const int group_1, const int group_2)
 	}
 }
 
-UnionFind::UnionFind(int size)
+template<typename ValueType>
+UnionFind<ValueType>::UnionFind(int size)
 {
 	_size = size;
 	_nodes = new Node*[size];
 	_groups = new GroupNode*[size];
 	_all_groups = new GroupNode*[size];
 	for (int i = 0; i < size; i++) {
-		_nodes[i] = new Node(i+1);
+		_nodes[i] = new Node();
 		_all_groups[i] = new GroupNode(_nodes[i]);
 		_groups[i] = _all_groups[i];
 	}
 }
 
-UnionFind::~UnionFind()
+template<typename ValueType>
+UnionFind<ValueType>::~UnionFind()
 {
 	for (int i = 0; i < _size; i++) {
 		delete _nodes[i];
